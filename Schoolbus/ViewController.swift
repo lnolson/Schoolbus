26ede56e43e5024f3f3f677df4851f258f31b1fd
Loren Olson <loren.olson@asu.edu>
2020-04-22 10:55:49 -0700

    Initial Commit

---
+++ Schoolbus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schoolbus
+//
+//  Created by Loren Olson on 4/22/20.
+//  Copyright © 2020 ASU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
